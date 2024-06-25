266a82c73f93a1ca0b59e800a92e3f538a7f3a9f
odhi99 <odhii9671@gmail.com>
2024-06-25 21:12:13 +0800

    3

--- detail.php
+++ detail.php
@@ -9,15 +9,13 @@ $data = mysqli_fetch_assoc($req);
 $now_date = Date('Y-m-d');
 $now_time = Date('H:i');
 
-if(isset($_SESSION['id'])){
+if (isset($_SESSION['id'])) {
     $id = $_SESSION['id'];
     $res_user = mysqli_query($conn, "SELECT nama, hp FROM tb_users WHERE role = 'user' AND id_user = '$id'");
     $data_user = mysqli_fetch_assoc($res_user);
-    
-}else{
+} else {
     $data_user['nama'] = '';
     $data_user['hp'] = '';
-
 }
 
 
@@ -38,8 +36,7 @@ if(isset($_SESSION['id'])){
     <script src="assets/jquery/dist/jquery.min.js"></script>
     <script src="assets/DataTables/datatables.js"></script>
 
-    <script type="text/javascript" src="https://app.sandbox.midtrans.com/snap/snap.js"
-        data-client-key="SB-Mid-client-Sh3wGKHVSP3NM6lF" async></script>
+    <script type="text/javascript" src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key="SB-Mid-client-Sh3wGKHVSP3NM6lF" async></script>
     <link rel="stylesheet" href="assets/DataTables/datatables.css">
 
     <!-- SWEETALERT -->
@@ -53,17 +50,17 @@ if(isset($_SESSION['id'])){
 
     <?php
     include("layout/navbar.php");
-    
+
     ?>
 
 
 
     <section id="detail-product">
-        <img src="assets/img/produk/<?= $data['foto']?>" alt="">
+        <img src="assets/img/produk/<?= $data['foto'] ?>" alt="">
         <div class="detail">
 
             <div class="text">
-                <div class="title">Produk <?= $data['id_produk']?></div>
+                <div class="title">Produk <?= $data['id_produk'] ?></div>
                 <div class="subtitle">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores nesciunt
                     blanditiis mollitia totam odio voluptates repellendus facere, maxime at fuga?</div>
             </div>
@@ -74,32 +71,32 @@ if(isset($_SESSION['id'])){
                 <li>Lorem ipsum dolor sit amet.</li>
             </ul>
             <div class="price">
-                Rp <?= number_format($data['harga'])?> / Jam
+                Rp <?= number_format($data['harga']) ?> / Jam
             </div>
 
         </div>
 
     </section>
 
- 
+
 
     <section id="reservasi">
 
         <div>
             <div class="input-form">
                 <p>Nama</p>
-                <input type="text" id="nama" value="<?= $data_user['nama']?>">
+                <input type="text" id="nama" value="<?= $data_user['nama'] ?>">
             </div>
 
             <div class="input-form">
                 <p>No. Hp</p>
-                <input type="text" id="hp" value="<?= $data_user['hp']?>">
+                <input type="text" id="hp" value="<?= $data_user['hp'] ?>">
             </div>
 
             <div class="divide-input">
                 <div class="input-form">
                     <p>Stok </p>
-                    <input type="number" min="1" value="<?= $data['jumlah']?>" readonly>
+                    <input type="number" min="1" value="<?= $data['jumlah'] ?>" readonly>
                 </div>
                 <div class="input-form">
                     <p>Jumlah </p>
@@ -108,11 +105,11 @@ if(isset($_SESSION['id'])){
             </div>
             <div class="input-form">
                 <p>Total Harga</p>
-                <input type="text" id="harga" value="<?=number_format($data['harga'])?>" readonly>
+                <input type="text" id="harga" value="<?= number_format($data['harga']) ?>" readonly>
             </div>
 
             <div class="input-form">
-                <button onclick="reserve()" id="reservasi-btn" data-id="<?= $data['id_produk']?>">Submit</button>
+                <button onclick="reserve()" id="reservasi-btn" data-id="<?= $data['id_produk'] ?>">Submit</button>
             </div>
 
 
@@ -126,15 +123,14 @@ if(isset($_SESSION['id'])){
 
 
     <script type="text/javascript">
+        function hitungHarga() {
+            let harga = <?= $data['harga'] ?>;
+            let jumlah = $('#jumlah').val()
 
-    function hitungHarga() {
-        let harga = <?= $data['harga']?>;
-        let jumlah = $('#jumlah').val()
-
-        document.getElementById('harga').value = (harga * jumlah).toLocaleString()
+            document.getElementById('harga').value = (harga * jumlah).toLocaleString()
 
 
-    }
+        }
     </script>
     <script src="reservasi.js"></script>
 
